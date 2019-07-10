63e4aa42714a02eb68f94952cc3672b51d76a5ad
Jake Gadaleta <jake.gads@gmail.com>
2019-07-10 16:02:29 -0400

    Setting up git cli

--- SRC/thirdAttempt/main.py
+++ SRC/thirdAttempt/main.py
@@ -5,6 +5,7 @@ from config import FONT_SIZE,  TOPSCORES
 
 
 book = None
+teams = []
 
 class Team():
     def __init__(self, number=None, name=None, scores=[]):
@@ -12,13 +13,12 @@ class Team():
         self.name = name
         self.scores = scores
 
-
     def genAverage(self):
-        
+
         orderedScores = []
         for i in self.scores:
             orderedScores.append(self.scores[i])
-        
+
         orderedScores.sort()
 
         sum = 0
@@ -30,13 +30,44 @@ class Team():
             except:
                 None
 
-        return sum / count  
+        return sum / count
+
 
 '''
 !!!!IMPORTANT!!!!
 Excel should be formatted as follows
 
-A page named Teams with colum A consisting of team numbers and colum B consting of Team names
+A sheet named "Teams" with colum A consisting of Team numbers and colum B consisting of Team names
+
+A sheet named "Entry" with colum A consisting of Team numbers and colum B consisting of scores colum C can mark the round but is not nessairy for this application to function
+
+These pages can be in any order and additional pages can be added as well
+if one or both tho the sheets are not there the program will not run (see example)
 '''
+
+
 def get_teams():
     book = xlrd.open_workbook(input('Excel File (can be dragged and dropped for windows)'))
+
+    sheet = book.sheet_by_name('Teams')
+
+    for i in range(sheet.nrows - 1):
+        try:
+            teams.append(Team(number=int(sheet.cell(i,0).value), name=sheet.cell(i,1).value))
+        except:
+            None
+
+    get_scores()
+
+
+def get_scores():
+    sheet = book.sheet_by_name('Entry')
+
+    for i in range(sheet.nrows - 1):
+        value = sheet.cell(i,0).value
+        if type(value) == type(1):
+            for team in teams:
+                if i is team.number:
+
+
+
