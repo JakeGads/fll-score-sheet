a24a20584c19377ad2a8300af8c34a5c9e8a3e9f
Jake Gadaleta <jake.gads@gmail.com>
2019-07-20 21:16:00 -0400

    Finished the scrolling going to add some css tommorow

--- SRC/thirdAttempt/static/gen/main.js
+++ SRC/thirdAttempt/static/gen/main.js
@@ -1,31 +1,27 @@
 function main(){
     var millisecondsToPause = 5000;
     var counter = 0;
+
+    
+
     var scroll = setInterval(function(){
         var now = new Date().getTime();
         while(now + millisecondsToPause > new Date().getTime()){}
 
-        if($(window).scrollTop() + $(window).height() != $(document).height()) {
-            console.log("lowering round " + counter)
+        var height = document.documentElement.scrollHeight;
+
+        if(counter <= height) {
+            console.log("lowering round " + counter);
             window.scrollBy(0,50);
-            counter += 1;
+            counter += 50;
         }
         else{
-            console.log("Resetting location and reloading the page")
-            $("html, body").animate({ scrollTop : 0});
-            document.location.reload(true);
+            console.log("Resetting location and reloading the page");
+            $("html, body").animate({ scrollTop : 0}, "fast");
+            document.location.reload(true)
         } 
         }, 
         200);
-}
-
-function getDocHeight() {
-    var D = document;
-    return Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
-        D.body.clientHeight, D.documentElement.clientHeight
-    );
 }
 
 main();
