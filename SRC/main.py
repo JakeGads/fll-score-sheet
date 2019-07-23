3b4bc20dad26230a07eae9e7509d5dced14aa644
Jake Gadaleta <jake.gads@gmail.com>
2019-07-22 21:36:55 -0400

    added comments

--- SRC/main.py
+++ SRC/main.py
@@ -1,17 +1,17 @@
-import xlrd
-import xlwt
-import logging
-from flask import Flask, render_template, Markup
-from flask_assets import Bundle, Environment
-from subprocess import call, check_output
-from tkinter import filedialog
-from config import FONT_SIZE,  TOPSCORES
+import xlrd # xl reading
+import logging #xl writing
+from flask import Flask, render_template, Markup # webserver, template renderer and str to markup converter
+from flask_assets import Bundle, Environment # js and cs bundlers
+from subprocess import call, check_output # allows to access terminal and to read the terminal output
+from tkinter import filedialog #file selections
+from config import FONT_SIZE, TOPSCORES # self made
 
 
+# sets these as the global variables
+book = None # The workbook that we will use to analyze
+teams = [] # Holds all of the teams 
 
-book = None
-teams = []
-
+# Team becomes a way to sort or to organize the order
 class Team():
     def __init__(self, number=None, name=None, scores='', average=None):
         self.number = number
@@ -31,13 +31,16 @@ These pages can be in any order and additional pages can be added as well
 if one or both tho the sheets are not there the program will not run (see example)
 '''
 
-def genAverage(arr):
+def genAverage(userList):
+    # this will reverse order a deep copy of the list
     orderedScores = []
-    for i in arr:
+    for i in userList:
         orderedScores.append(i)
 
     orderedScores.sort(reverse=True)
 
+    # calculates the current average up to the user defined value
+    # if there is no value it will avoid a divide by zero instance which would cause a server crash 
     sum = 0
     count = 0
     for i in range(TOPSCORES):
@@ -52,6 +55,7 @@ def genAverage(arr):
 
 
 def get_teams():
+    # reads through the team list sheet in order to start making the team list
     sheet = book.sheet_by_name('Teams')
 
     for i in range(sheet.nrows - 1):
@@ -65,6 +69,7 @@ def get_teams():
 
 
 def get_scores():
+    #looks through the entry
     sheet = book.sheet_by_name('Entry')
     # I realize that this is very un-optimized but this has something to do with memory management
     try:
@@ -93,12 +98,14 @@ def sortTeams():
         # Swap the found minimum element with the first element         
         teams[i], teams[max_idx] = teams[max_idx], teams[i]
 
+# webserver info
 app = Flask(__name__)
-
+# gets the javascript ready to import
 js = Bundle('main.js', output='gen/main.js')
 assets = Environment(app)
 assets.register('main_js', js)
 
+# the home (and only) page
 @app.route('/')
 def updateScoreBoard():
     # auto scroll JS Command “var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 2000);”
@@ -108,6 +115,7 @@ def updateScoreBoard():
     sortTeams()
 
     table = ''
+    # goes through each team and makes an html row out of them
     for pos in range(len(teams)):
         table += '''
         <tr>
@@ -125,20 +133,23 @@ def updateScoreBoard():
             average = round(teams[pos].average, 2)
         )
 
+    # returns the html from main.html with the table running through markup and the average score
     return render_template('main.html', table=Markup(table), average=TOPSCORES)
 
-    
+# This function is left blank as I have not figured out how to run this at the destruction
 def finalize():
     None
 
-
+# The main
 if __name__ == "__main__":
+    # sets the book to what the user selects in the tk dialogue
     book = xlrd.open_workbook(filedialog.askopenfilename(initialdir = "/",title = "Select File",filetypes = (("xlsx files","*.xlsx"),("xls files","*.xls"),("all files","*.*"))))
     
-
-    #book = xlrd.open_workbook('example.xls')
+    # the testing book
+    # book = xlrd.open_workbook('example.xls')
     
-    
+    # gets the team
+    # note if a team is added the program must be restarted
     get_teams()
 
     try:
@@ -147,7 +158,7 @@ if __name__ == "__main__":
     except:
         try:
             output = check_output('netstat  -ano  |  findstr  5000')
-            processID = output[-4:]
+            processID = str(output)[-4:]
             call(' taskkill  /F  /PID  {processID}'.format(processID=processID))
             print('killed the windows port')
         except:
