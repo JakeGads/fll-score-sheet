ac665ba66dc24683689d259d84d4010eb0405865
Jake Gadaleta <jake.gads@gmail.com>
2020-07-12 20:30:29 -0400

    finished the sorting algorithm

--- src/team.ts
+++ src/team.ts
@@ -20,18 +20,18 @@ export default class Team {
   public generateAverage(): void {
     let sum = 0;
 
-    if (this.scores.length === 0) {
+    if (this.scores.array.lengthgth === 0) {
       this.average = 0;
-    } else if (this.scores.length < topNum) {
-      for (let i = 0; i < this.scores.length; i += 1) {
+    } else if (this.scores.array.lengthgth < topNum) {
+      for (let i = 0; i < this.scores.array.lengthgth; i += 1) {
         sum += this.scores[i];
       }
       this.average = sum / topNum;
-    } else if (this.scores.length >= topNum) {
+    } else if (this.scores.array.lengthgth >= topNum) {
       const copy: Array<number> = [];
       // note we need to do a deep copy to ensure the integrity of the original array
 
-      for (let i = 0; i < this.scores.length; i += 1) {
+      for (let i = 0; i < this.scores.array.lengthgth; i += 1) {
         copy.push(this.scores[i]);
       }
 
@@ -44,4 +44,25 @@ export default class Team {
       this.average = sum / topNum;
     }
   }
+}
+
+export function sortTeams(array: Array<Team>) {
+  const tempArray = new Array<Team>();
+
+  for (let i = 0; i < array.length; i += 1) {
+    array[i].generateAverage();
+    tempArray.push(array[i]);
+  }
+
+  for (let i = 0; i < tempArray.length; i += 1) {
+    for (let j = 0; j < tempArray.length; j += 1) {
+      if (tempArray[j].average > tempArray[j + 1].average) {
+        const tmp = tempArray[j];
+        tempArray[j] = tempArray[j + 1];
+        tempArray[j + 1] = tmp;
+      }
+    }
+  }
+
+  return tempArray;
 }
