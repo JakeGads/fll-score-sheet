ddc0be4501443931db4b702d3f5e0a682d04ffd7
Jake Gadaleta <jake.gads@gmail.com>
2020-07-18 18:51:10 -0400

    Syncing

--- src/team.ts
+++ src/team.ts
@@ -20,18 +20,18 @@ export default class Team {
   public generateAverage(): void {
     let sum = 0;
 
-    if (this.scores.array.lengthgth === 0) {
+    if (this.scores.length === 0) {
       this.average = 0;
-    } else if (this.scores.array.lengthgth < topNum) {
-      for (let i = 0; i < this.scores.array.lengthgth; i += 1) {
+    } else if (this.scores.length < topNum) {
+      for (let i = 0; i < this.scores.length; i += 1) {
         sum += this.scores[i];
       }
       this.average = sum / topNum;
-    } else if (this.scores.array.lengthgth >= topNum) {
+    } else if (this.scores.length >= topNum) {
       const copy: Array<number> = [];
       // note we need to do a deep copy to ensure the integrity of the original array
 
-      for (let i = 0; i < this.scores.array.lengthgth; i += 1) {
+      for (let i = 0; i < this.scores.length; i += 1) {
         copy.push(this.scores[i]);
       }
 
